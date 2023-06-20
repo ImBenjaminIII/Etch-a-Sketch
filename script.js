7192839712cd06301f3522f44c45496e9ec870ae
Benjamin <angelmoreno.am205@gmail.com>
2023-06-20 16:36:53 +0800

    adding javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+const board = document.querySelector('#board');
+const colorBtn = document.querySelector('.color');
+const eraserBtn = document.querySelector('.eraser');
+const clearBtn = document.querySelector('.clear');
+const slider = document.querySelector('#slider');
+const inputSlider = document.querySelector('#slider-container .input');
+
+// button event listeners
+colorBtn.addEventListener('click', () => {});
+eraserBtn.addEventListener('click', () => {});
+clearBtn.addEventListener('click', () => {});
+
+inputSlider.textContent = '10 x 10';
