ae5522456f5e80f5f6eecf28639e6e21343ddd5f
Benjamin <angelmoreno.am205@gmail.com>
2023-06-25 14:43:15 +0800

    adding functionality to the buttons and title color

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ function setupBoard(size) {
     const gridElement = document.createElement('div');
     gridElement.classList.add('grid-elements');
     gridElement.style.border = '1px solid black';
-    gridElement.addEventListener('mouseover', changeColor);
     gridElement.addEventListener('mousedown', changeColor);
+    gridElement.addEventListener('mouseover', changeColor);
     board.appendChild(gridElement);
   }
 }
@@ -110,6 +110,48 @@ sizeSlider.oninput = () => {
 sizeSlider.onchange = () => {
   setupBoard(sizeSlider.value);
 };
+
+// random color title
+const title = document.querySelector('.title').innerHTML.split('');
+
+// Converts integer to hex
+const colToHex = c => {
+  // Hack so colors are bright enough
+  let color = c < 75 ? c + 75 : c;
+  let hex = color.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+};
+
+// uses colToHex to concatenate
+// a full 6 digit hex code
+const rgbToHex = (r, g, b) => {
+  return '#' + colToHex(r) + colToHex(g) + colToHex(b);
+};
+
+// Returns three random 0-255 integers
+const getRandomColor = () => {
+  return rgbToHex(
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256)
+  );
+};
+
+// This is the prototype function
+// that changes the color of each
+// letter by wrapping it in a span
+// element.
+Array.prototype.randomColor = function () {
+  let html = '';
+  this.map(letter => {
+    let color = getRandomColor();
+    html += '<span style="color:' + color + '">' + letter + '</span>';
+  });
+  return html;
+};
+
+// Set the text
+document.querySelector('.title').innerHTML = title.randomColor();
 
 // set the board size on the opening
 window.onload = () => {
